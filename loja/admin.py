e5e5325a9aeabab2ee33ce01e7e724f2f8248bfd
Rogério Soares <rogerio_si@yahoo.com.br>
2021-09-13 13:39:26 +0000

    admin

--- loja/admin.py
+++ loja/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Produtos)
-admin.site.register(Carrinho)
-admin.site.register(Clientes)
+#admin.site.register(Produtos)
+#admin.site.register(Carrinho)
+#admin.site.register(Clientes)
+
+
+@admin.register(Produtos)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("nome", "descricao","valor","image")
+    list_filter = ("valor", )
+    search_fields = ("nome__startswith", )
+
+@admin.register(Carrinho)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("chave", "pedido","valor","qtd","sub_total")
+    list_filter = ("chave", )
+    search_fields = ("chave__startswith", )
+
+@admin.register(Clientes)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("pedido", "nome","pagamento","endereco")
+    list_filter = ("pedido", )
+    search_fields = ("pedido__startswith", )
--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
